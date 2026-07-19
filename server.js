0961f5be4ed13190c3f186363ffcb98aa51fcccb
pavi-200 <sreekarampavithra@gmail.com>
2026-07-19 10:51:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(
+  "/api/auth",
+  require("./routes/authRoutes")
+);
+
+app.use(
+  "/api/tasks",
+  require("./routes/taskRoutes")
+);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+
+    console.log("MongoDB Connected");
+
+    app.listen(
+      process.env.PORT,
+      () => {
+
+        console.log(
+          `Server Running on Port ${process.env.PORT}`
+        );
+
+      }
+    );
+
+  })
+  .catch(err =>
+    console.log(err)
+  );
