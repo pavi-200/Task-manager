6c83b9eedc86494729291f905048efba33f285e7
pavi-200 <sreekarampavithra@gmail.com>
2026-07-19 10:49:39 +0530

    Add files via upload

---
+++ routes/authRoutes.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+
+  const { username, email, password } = req.body;
+
+  const existingUser =
+    await User.findOne({ email });
+
+  if (existingUser) {
+    return res.json({
+      message: "Email already exists"
+    });
+  }
+
+  const hashedPassword =
+    await bcrypt.hash(password, 10);
+
+  const user = new User({
+    username,
+    email,
+    password: hashedPassword
+  });
+
+  await user.save();
+
+  res.json({
+    message: "User Registered"
+  });
+});
+
+router.post("/login", async (req, res) => {
+
+  const { email, password } = req.body;
+
+  const user =
+    await User.findOne({ email });
+
+  if (!user) {
+    return res.json({
+      message: "User Not Found"
+    });
+  }
+
+  const valid =
+    await bcrypt.compare(
+      password,
+      user.password
+    );
+
+  if (!valid) {
+    return res.json({
+      message: "Wrong Password"
+    });
+  }
+
+  const token = jwt.sign(
+    { id: user._id },
+    process.env.JWT_SECRET
+  );
+
+  res.json({ token });
+});
+
+module.exports = router;
